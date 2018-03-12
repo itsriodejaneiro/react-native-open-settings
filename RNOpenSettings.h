952440d114837da037899362722c59f77c22a777
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-03-12 15:51:57 -0300

    Support RN 0.40+

--- RNOpenSettings.h
+++ RNOpenSettings.h
@@ -7,7 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <RCTBridge.h>
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include("RCTBridge.h")
+#import "RCTBridge.h"
+#else
+#import "React/RCTBridge.h"   // Required when used as a Pod in a Swift project
+#endif
 
 @interface RNOpenSettings : NSObject <RCTBridgeModule>
 
